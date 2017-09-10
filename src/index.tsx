7dc075b9e5b299992bdb8a51ec5d7155f3426a0a
DiyanaDimitrova <H_E_R_A_92@abv.bg>
2017-09-10 15:49:09 +0300

    heroku fixes

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,16 @@ import AppContainer from './containers/AppContainer/AppContainer'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import * as injectTapEventPlugin from 'react-tap-event-plugin'
 import axios from 'axios'
-
 import { Provider as ReduxProvider } from 'react-redux'
 import { appStore } from './store/AppStore'
-
+const config = require('./server/config/default.json')
 injectTapEventPlugin()
-
 // setup global axios defaults
 var instance = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: process.env.NODE_ENV === 'development' ? config.organizeMeDev.server : config.organizeMeProd.server //'http://localhost:3001'
 })
 axios.defaults.timeout = 6000
-axios.defaults.baseURL = 'http://localhost:3001'
+axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? config.organizeMeDev.server : config.organizeMeProd.server //'http://localhost:3001'
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 ReactDOM.render(
